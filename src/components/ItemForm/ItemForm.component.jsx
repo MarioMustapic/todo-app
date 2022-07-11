bfa664dff25d80b742a96290d233917fd228ae5f
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-07-11 20:40:59 +0200

    Add Item model

--- src/components/ItemForm/ItemForm.component.jsx
+++ src/components/ItemForm/ItemForm.component.jsx
@@ -1,9 +1,10 @@
-export function ItemForm (props) {
-    return (
-        <form onSubmit={props.onSubmit}>
-          <input type="text" placeholder="TODO: buy milk"
-          name={props.name} value={props.value} onChange={props.onChange} />
-          <button type="submit">Submit</button>
-        </form>
-    )
+export function ItemForm(props) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <input type="text" placeholder="TODO: Buy milk"
+        name="item" value={props.value} onChange={props.onChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
