0d7cd81705dccee920ba0655de8cb0d49402777f
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-07-11 20:01:03 +0200

    Added ItemForm component

---
+++ src/components/ItemForm/ItemForm.jsx
@@ -0,0 +1,8 @@
+export function ItemForm (props) {
+    return (
+        <form type="text">
+          <input type="text" />
+          <button type="submit">Submit</button>
+        </form>
+    )
+}
