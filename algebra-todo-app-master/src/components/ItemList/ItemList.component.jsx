6c1d3b43608593013a5f3ff8ed5ef3ad604f8be0
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-07-12 18:47:23 +0200

    Add item delete feature

---
+++ algebra-todo-app-master/src/components/ItemList/ItemList.component.jsx
@@ -0,0 +1,17 @@
+export function ItemList(props) {
+  const itemElements = props.items.map((item) => (
+    <div key={item.id}>
+      <input type="checkbox" name="" id="" />
+      <span>{item.text} ({item.displayCreatedAt()})</span>
+      <button type="button" onClick={props.onDelete(item)}>
+        Delete
+      </button>
+    </div>
+  ));
+
+  return (
+    <div>
+      {itemElements}
+    </div>
+  );
+}
